ed7e7c068a98ace1e6aeb9c3592c2e4e3f4da7f1
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-02-01 13:14:29 -0500

    Moved all of the Main menu information into its own class.

---
+++ src/main/java/MainMenu.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+public class MainMenu {
+
+    public MainMenu() {}
+
+
+
+    public void introMessage() {
+        // Introductory Message.
+
+        System.out.println("\n\nThanks for playing Terminal-Tac-Toe!");
+        System.out.println("Select a game mode, single player (1p) or multiplayer (2p).");
+    }
+
+
+    public int gameModeSelection() {
+        // Loops until the user selects the proper game mode, then returns it.
+
+        int mode = 0; // 0 for unselected game mode, 1 for single player, 2 for multiplayer.
+        boolean validSelection = false;
+        while (!validSelection) {
+
+            // Prompts the user to select a game mode.
+            Scanner ModeSelection = new Scanner(System.in);
+            String gameMode;
+            System.out.println("\nPlease input either 1p or 2p: ");
+            gameMode = ModeSelection.nextLine();
+
+            // Checks which game mode was selected, and if the selected game mode is valid.
+            if (gameMode.equals("1p") || gameMode.equals("1P")) {
+                System.out.println("You selected: Single Player!");
+                mode = 1;
+                validSelection = true;
+
+            } else if (gameMode.equals("2p") || gameMode.equals("2P")) {
+                System.out.println("You selected: Multiplayer!");
+                mode = 2;
+                validSelection = true;
+
+            } else {
+                System.out.println("Invalid game mode.");
+            }
+        }
+        return mode;
+    }
+
+
+    public int runMainMenu() {
+        // Runs the main menu when called.
+
+        introMessage();
+        int mode = gameModeSelection();
+        return mode;
+    }
+
+}
