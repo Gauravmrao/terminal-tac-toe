4f02ef7f259241b37ac8ae39db9cea8b41636b69
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-02-01 15:35:24 -0500

    Finished the first draft of the board class.

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -13,6 +13,7 @@ public class Board {
         }
     }
 
+
     public String getElement(String index){
         // Returns the element in the board at a specified index.
 
@@ -30,9 +31,37 @@ public class Board {
         };
     }
 
-    public void updateBoard(String playerMove, String player) {
 
+    public void setElement(String index, String player){
+        // Sets the element in the board at a specified index with the input player value.
 
+        switch (index) {
+            case "A1", "a1": board[0][0] = player;
+            case "A2", "a2": board[0][1] = player;
+            case "A3", "a3": board[0][2] = player;
+            case "B1", "b1": board[1][0] = player;
+            case "B2", "b2": board[1][1] = player;
+            case "B3", "b3": board[1][2] = player;
+            case "C1", "c1": board[2][0] = player;
+            case "C2", "c2": board[2][1] = player;
+            case "C3", "c3": board[2][2] = player;
+        }
     }
+
+
+
+    public int updateBoard(String playerMove, String player) {
+        // Updates the board with the player's move.
+
+        if (getElement(playerMove).equals(" ")) {
+            setElement(playerMove, player);
+            return 1;
+        } else {
+            System.out.println("That spot has already been taken, choose a different spot!");
+            return 0;
+        }
+    }
+
+
 
 }
