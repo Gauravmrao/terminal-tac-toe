2f6116f86859bfcfc262278a6109ddddc40808d4
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-05-16 16:04:09 +0530

    finished multiplayer working functionality for now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,36 +2,23 @@
 public class Main {
 
 
-
-    // TODO:
-    // Implement the single player and the multiplayer functions
-    // Based on the value of "mode", the correct function should be called.
-
-
-
     // The main function
     public static void main(String[] args) {
-
 
         // Run the main menu
         MainMenu menu = new MainMenu();
         int gameMode = menu.runMainMenu();
 
+        // run the selected game mode
         Board board = new Board();
         if (gameMode == 2) {
             Multiplayer multiplayer = new Multiplayer(board);
             multiplayer.playMultiplayer();
+        } else {
+
         }
 
 
-
-        // Start a game
-        // Game game = new Game(gameMode);
-        // game.gameModeSelection();
-
-
-        // Board board = new Board();
-        // board.showBoard();
 
     }
 
